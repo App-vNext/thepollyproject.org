c6111ea6aa541521beec7a37b967333ad4338ff5
joelhulen <joelhulen@gmail.com>
2026-07-08 12:14:23 -0400

    Drop Minima theme; add custom site shell with dark-mode toggle

---
+++ assets/js/theme-toggle.js
@@ -0,0 +1,11 @@
+(function () {
+  var STORAGE_KEY = "theme";
+  var button = document.querySelector(".theme-toggle");
+  if (!button) return;
+  button.addEventListener("click", function () {
+    var current = document.documentElement.getAttribute("data-theme");
+    var next = current === "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-theme", next);
+    localStorage.setItem(STORAGE_KEY, next);
+  });
+})();
